ae289b1a97df3f7a2232b32964522c8452a87be1
Julaiti Alafate <jalafate@gmail.com>
2018-06-20 18:50:51 -0700

    filtered out cargo logs in the example

--- examples/find-prime-nums-scripts/find-primes.sh
+++ examples/find-prime-nums-scripts/find-primes.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 cd /home/ubuntu/workspace/rust-tmsn
-killall cargo
-killall rustc
-killall find-prime-nums
+killall cargo 2> /dev/null
+killall rustc 2> /dev/null
+killall find-prime-nums 2> /dev/null
 git checkout -- .
 git pull
-cargo build --release
-RUST_LOG=debug cargo run --example find-prime-nums
+cargo build --release 2> /dev/null
+RUST_LOG=debug,cargo=error cargo run --example find-prime-nums
