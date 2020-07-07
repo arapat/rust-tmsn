8bad6810909a84aca39a6e629252f7093f9a3f0b
Julaiti Alafate <jalafate@gmail.com>
2020-07-07 08:58:02 -0700

    updates

---
+++ tmsn/packet/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["PacketType","Types of packets"]],"struct":[["Packet","Packet "]]});
--- tmsn/sidebar-items.js
+++ tmsn/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Network","A structure for communicating over the network in an asynchronous, non-blocking manner"]]});
+initSidebarItems({"mod":[["packet","The packet sent out via network"]],"struct":[["Network","A structure for communicating over the network in an asynchronous, non-blocking manner"]]});
